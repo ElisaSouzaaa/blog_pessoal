739cd4264c106fb27c82ad5e1440ba61ab8d8385
Elisa Souza <contato.elisasouza@hotmail.com>
2023-10-18 15:22:21 -0300

    Deploy do Blog Pessoal

--- src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     public Optional<Usuario> findByUsuario(@Param("usuario") String usuario);
 }
